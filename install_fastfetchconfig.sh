f55d790c99cbe0fc98e3c338b6acf3852b241e9f
Trihedraf <forrest@mailfsh.com>
2024-07-18 21:14:17 -0700

    mkdir for fastfetch config

--- install_fastfetchconfig.sh
+++ install_fastfetchconfig.sh
@@ -1,2 +1,3 @@
 [ ! -f ~/.config/fastfetch/config.jsonc ] || mv ~/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc.bak
+mkdir -p ~/.config/fastfetch/
 cp .config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
