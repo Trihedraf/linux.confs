732b7d13894a33c488acbef0257733db55b472b4
Trihedraf <forrest@mailfsh.com>
2024-10-01 21:59:34 -0700

    add sh shebang to install_fastfetchconfig.sh

--- install_fastfetchconfig.sh
+++ install_fastfetchconfig.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 [ ! -f ~/.config/fastfetch/config.jsonc ] || mv ~/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc.bak
 mkdir -p ~/.config/fastfetch/
 cp .config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
