e89f79c649f31f41624c5c08dbcea5347f62f3dd
Trihedraf <forrest@mailfsh.com>
2024-10-12 00:48:09 -0700

    +Font Menu +Native Package Menu #Appimage Menu

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+checkPackageManager() {
+    ## Check Package Manager
+    PACKAGEMANAGER=$1
+    for pgm in ${PACKAGEMANAGER}; do
+        if command_exists "${pgm}"; then
+            PACKAGER=${pgm}
+            printf "%b\n" "${CYAN}Using ${pgm} as package manager${RC}"
+            break
+        fi
+    done
+    
+    if [ -z "$PACKAGER" ]; then
+        printf "%b\n" "${RED}Can't find a supported package manager${RC}"
+        exit 1
+    fi
+}
+
 bash_menu()
 {
     while true; do
@@ -69,19 +90,49 @@ bash_menu()
     done
 }
 
-appimage_menu()
+native_package_menu()
 {
     while true; do
         # Display the menu
         clear
         printf "\n  Please select an option:\n\n"
+        printf "    1. Install Alacritty\n"
         printf "    0. Return to main menu\n"
         printf "\n  Enter your choice (0-1): "
-        read -r appimage_choice
+        read -r package_manager_choice
         printf "\n\n"
         
-        case "$appimage_choice" in
+        case "$package_manager_choice" in
             1)
+                checkPackageManager "apt-get dnf pacman nala zypper"
+                case "$PACKAGER" in
+                    apt-get|nala)
+                        sudo "$PACKAGER" update
+                        sudo "$PACKAGER" install -y alacritty
+                        printf "\nAlacritty has been installed.\n"
+                        printf "\n  Press enter to continue..."
+                        read -r package_manager_choice
+                    ;;
+                    pacman)
+                        sudo "$PACKAGER" -S --noconfirm alacritty
+                        printf "\nAlacritty has been installed.\n"
+                        printf "\n  Press enter to continue..."
+                        read -r package_manager_choice
+                    ;;
+                    dnf)
+                        sudo "$PACKAGER" update
+                        sudo "$PACKAGER" install -y alacritty
+                        printf "\nAlacritty has been installed.\n"
+                        printf "\n  Press enter to continue..."
+                        read -r package_manager_choice
+                    ;;
+                    zypper)
+                        sudo "$PACKAGER" --non-interactive install -y alacritty
+                        printf "\nAlacritty has been installed.\n"
+                        printf "\n  Press enter to continue..."
+                        read -r package_manager_choice
+                    ;;
+                esac
             ;;
             0)
                 break
@@ -90,23 +141,64 @@ appimage_menu()
     done
 }
 
+#appimage_menu()
+#{
+#    while true; do
+#        # Display the menu
+#        clear
+#        printf "\n  Please select an option:\n\n"
+#        printf "    1. \n"
+#        printf "    0. Return to main menu\n"
+#        printf "\n  Enter your choice (0-1): "
+#        read -r appimage_choice
+#        printf "\n\n"
+
+#        case "$appimage_choice" in
+#            1)
+#                fdsa
+#            ;;
+#            0)
+#                break
+#            ;;
+#        esac
+#    done
+#}
+
+font_menu()
 {
     while true; do
-        # Display the menu
+        #Display the menu
         clear
         printf "\n  Please select an option:\n\n"
+        printf "    1. Install CommitMono Nerd Font\n"
+        printf "    2. Install Meslo LGS Nerd Font\n"
+        printf "    3. Install Ubuntu Mono Nerd Font\n"
         printf "    0. Return to main menu\n"
-        printf "\n  Enter your choice (0-4): "
+        printf "\n  Enter your choice (0-3): "
+        read -r font_choice
         printf "\n\n"
         
+        case "$font_choice" in
             1)
+                mkdir -p ~/.local/share/fonts/commitmono-nerd
+                wget -q --show-progress --progress=bar:force:noscroll https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.zip -O - | bsdtar -xvf- -C ~/.local/share/fonts/commitmono-nerd "CommitMonoNerdFontMono-*.otf"
+                printf "CommitMono Nerd Font has been installed.\n"
                 printf "\n  Press enter to continue..."
+                read -r font_choice
             ;;
             2)
+                mkdir -p ~/.local/share/fonts/meslo-lgs-nerd
+                wget -q --show-progress --progress=bar:force:noscroll https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip -O - | bsdtar -xvf- -C ~/.local/share/fonts/meslo-lgs-nerd "MesloLGSNerdFontMono-*.ttf"
+                printf "Meslo Nerd Font has been installed.\n"
                 printf "\n  Press enter to continue..."
+                read -r font_choice
             ;;
             3)
+                mkdir -p ~/.local/share/fonts/ubuntu-mono-nerd
+                wget -q --show-progress --progress=bar:force:noscroll https://github.com/ryanoasis/nerd-fonts/releases/latest/download/UbuntuMono.zip -O - | bsdtar -xvf- -C ~/.local/share/fonts/ubuntu-mono-nerd "UbuntuMonoNerdFontMono-*.ttf"
+                printf "Ubuntu Mono Nerd Font has been installed.\n"
                 printf "\n  Press enter to continue..."
+                read -r font_choice
             ;;
             0)
                 break
@@ -170,10 +262,12 @@ while true; do
     clear
     printf "\n  Please select an option:\n\n"
     printf "    1. Bash files menu\n"
-    printf "    2. AppImage menu\n"
+    printf "    2. Native Package menu\n"
+    #    printf "    3. AppImage menu\n"
+    printf "    3. Font menu\n"
     printf "    4. Config menu\n"
     printf "    0. Exit\n"
-    printf "\n  Enter your choice (0-3): "
+    printf "\n  Enter your choice (0-4): "
     read -r choice
     printf "\n\n"
     
@@ -182,9 +276,13 @@ while true; do
             bash_menu
         ;;
         2)
-            appimage_menu
+            native_package_menu
         ;;
+        #        3)
+        #            appimage_menu
+        #        ;;
         3)
+            font_menu
         ;;
         4)
             config_menu
