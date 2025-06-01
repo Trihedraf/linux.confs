74e8a33c1ac09f06b51f67e2eeab45930071f00c
Trihedraf <forrest@mailfsh.com>
2025-06-01 16:51:09 -0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,11 +1,11 @@
 sudo sh -c 'echo "" > /etc/motd'
 sudo apt-get install -y git micro curl wget rsync screen cockpit samba
-mkdir ~/git &&\
-    git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs &&\
-    cd ~/git/linux.confs &&\
-    chmod +x ./install.sh &&\
-    echo -e "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh &&\
-    sudo cp -r debian-trixie/etc/* /etc/
+mkdir -p ~/git
+git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs
+cd ~/git/linux.confs
+chmod +x ./install.sh
+echo -e "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
+sudo cp -r debian-trixie/etc/* /etc/
 sudo apt update
 sudo apt-get install -y linux-headers-amd64 zfsutils-linux zfs-auto-snapshot
 git clone https://github.com/45drives/cockpit-zfs-manager.git ~/git/cockpit-zfs &&\
