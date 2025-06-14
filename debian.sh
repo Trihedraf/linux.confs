83a5dd53b6df23eaba5cd893f54a8e113dde5b8e
Trihedraf <forrest@mailfsh.com>
2025-06-14 13:37:00 -0700

    reordering debian install

--- debian.sh
+++ debian.sh
@@ -3,15 +3,15 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cockpit curl git ethtool 
 mkdir -pv ~/git
 git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs
 cd ~/git/linux.confs
+sudo cp -rv debian-trixie/etc/* /etc/ && sudo rm /etc/apt/sources.list
+sudo apt-get update
 chmod +x ./install.sh
 echo -e "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
-sudo cp -rv debian-trixie/etc/* /etc/ && sudo rm /etc/apt/sources.list
-sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-amd64 zfsutils-linux zfs-auto-snapshot
 pipx install zfs-autobackup
 git clone https://github.com/45drives/cockpit-zfs-manager.git ~/git/cockpit-zfs &&\
-    sudo systemctl enable cockpit.socket &&\
-    sudo cp -rv ~/git/cockpit-zfs/zfs /usr/share/cockpit
+    sudo cp -rv ~/git/cockpit-zfs/zfs /usr/share/cockpit &&\
+    sudo systemctl enable cockpit.socket
 if ! command -v docker &> /dev/null; then curl -fsSL https://get.docker.com | sh; fi && sudo usermod -aG docker $(whoami)
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
