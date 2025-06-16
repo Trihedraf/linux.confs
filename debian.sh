898edd28b1d97129ef14767f095edd5014f5931f
Trihedraf <forrest@mailfsh.com>
2025-06-15 21:08:04 -0700

    Add zfs-load-key systemd service

--- debian.sh
+++ debian.sh
@@ -15,5 +15,6 @@ git clone https://github.com/45drives/cockpit-zfs-manager.git ~/git/cockpit-zfs 
 if ! command -v docker &> /dev/null; then curl -fsSL https://get.docker.com | sh; fi && sudo usermod -aG docker $(whoami)
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+sudo systemctl enable --now zfs-load-key.service
 sudo systemctl enable udpgroforwarding.service && echo "Please change your NIC from eth0 to correct id in /etc/systemd/system/udpgroforwarding.service then start udpgroforwarding.service"
 echo "Please add nameserver ip_address to /etc/resolvconf/resolv.conf.d/base"
