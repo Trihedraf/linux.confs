2015cb9faed9b3cacada9830a0cf467d763998ff
Trihedraf <forrest@mailfsh.com>
2025-07-09 13:27:37 -0700

    Create arch.sh

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 git clone https://github.com/Trihedraf/linux.confs && cd linux.confs && ./install.sh
 ```
 
+# Arch System Setup Script
+```bash
+curl -fsSL https://raw.githubusercontent.com/Trihedraf/linux.confs/refs/heads/master/arch.sh | sh
+```
+
 # Debian Trixie System Setup Script
 ```bash
 sudo apt install curl && curl -fsSL https://raw.githubusercontent.com/Trihedraf/linux.confs/refs/heads/master/debian.sh | sh
---
+++ arch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo pacman -S --noconfirm --needed base-devel cmake docker docker-buildx \
+    docker-compose fastfetch flatpak giflib lib32-giflib github-cli git gnutls \
+    lib32-gnutls libpng lib32-libpng make man-db micro ncurses \
+    lib32-ncurses screen shellcheck sudo superfile sqlite lib32-sqlite tar \
+    tree tldr trash-cli unzip wget zip 
+
+sudo flatpak remote-add --if-not-exists flathub \
+    https://flathub.org/repo/flathub.flatpakrepo
+
+mkdir -pv ~/git
+git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs &&\
+    cd ~/git/linux.confs &&\
+    chmod +x ./install.sh &&\
+    printf "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
+
+git clone https://aur.archlinux.org/paru.git ~/git/paru &&\
+    cd ~/git/paru &&\
+    makepkg -si --noconfirm --needed &&\
+    printf "[options]\nSkipReview\n" > ~/.config/paru/paru.conf
+
+paru -S multitail
