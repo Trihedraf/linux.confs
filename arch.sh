a216159f15c48d3e2a1793414e52d143e434d86f
Trihedraf <forrest@mailfsh.com>
2025-07-20 15:11:13 -0700

    switch distro scripts to install zsh configs

--- arch.sh
+++ arch.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm --needed base-devel cmake docker docker-buildx \
     docker-compose fastfetch flatpak giflib lib32-giflib git github-cli gnutls \
     lib32-gnutls libpng lib32-libpng linux-tools man-db micro mingw-w64 \
     ncurses lib32-ncurses screen shellcheck superfile sqlite lib32-sqlite tar \
-    tree tldr trash-cli unzip wget zip 
+    tree tldr trash-cli unzip wget zip zsh
 
 sudo flatpak remote-add --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
@@ -14,7 +14,7 @@ mkdir -pv ~/git
 git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs &&\
     cd ~/git/linux.confs &&\
     chmod +x ./install.sh &&\
-    printf "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
+    printf "1\n6\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
 
 git clone https://aur.archlinux.org/paru.git ~/git/paru &&\
     cd ~/git/paru &&\
--- debian.sh
+++ debian.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 sudo sh -c 'echo "" > /etc/motd'
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cockpit curl git ethtool iperf3 micro net-tools pipx resolvconf rsync samba screen shellcheck wget
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cockpit curl git ethtool iperf3 micro net-tools pipx resolvconf rsync samba screen shellcheck wget zsh
 sudo resolvconf -u
 mkdir -pv ~/git
 git clone https://github.com/Trihedraf/linux.confs ~/git/linux.confs
@@ -7,7 +8,7 @@ cd ~/git/linux.confs
 sudo cp -rv debian-trixie/etc/* /etc/ && sudo rm /etc/apt/sources.list
 sudo apt-get update
 chmod +x ./install.sh
-echo -e "1\n1\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
+echo -e "1\n6\n\n0\n3\n4\n\n5\n\n0\n4\n3\n\n0\n0\n" | ./install.sh
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-amd64 zfsutils-linux zfs-auto-snapshot
 sudo pipx install --global zfs-autobackup
 git clone https://github.com/45drives/cockpit-zfs-manager.git ~/git/cockpit-zfs &&\
