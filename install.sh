cf1345537347f542ed5305832fee784d3ada1546
Trihedraf <forrest@mailfsh.com>
2025-08-26 05:05:22 -0700

    install all install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,7 @@
 #!/bin/sh
+GITPATH=$(cd "$(dirname "$(realpath "$0")")" && pwd)
 
-. ./scripts/configFiles.sh
-. ./scripts/devilutionxDesktop.sh
-. ./scripts/fontInstall.sh
-. ./scripts/shellConf.sh
-
-while true; do
-    # Display the menu
-    clear
-    printf "\n  Please select an option:\n\n"
-    printf "    1. Shell files menu\n"
-    printf "    2. Font menu\n"
-    printf "    3. Config menu\n"
-    printf "    4. DevilutionX.desktop\n"
-    printf "    0. Exit\n"
-    printf "\n  Enter your choice (0-4): "
-    read -r choice
-    printf "\n\n"
-
-    case "$choice" in
-        1)
-            shell_menu
-        ;;
-        2)
-            font_menu
-        ;;
-        3)
-            config_menu
-        ;;
-        4)
-            devilutionx_desktop
-            printf "\n  Press enter to continue..."
-            read -r choice
-        ;;
-        0)
-            exit 0
-        ;;
-    esac
-done
+"$GITPATH/scripts/configFiles.sh" -det
+"$GITPATH/scripts/devilutionxDesktop.sh"
+"$GITPATH/scripts/fontInstall.sh"
+"$GITPATH/scripts/shellConf.sh"
