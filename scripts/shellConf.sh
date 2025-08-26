e79f4a6fd6e591a981f59df09298fa7253aa7e10
Trihedraf <forrest@mailfsh.com>
2025-08-26 05:05:06 -0700

    shellConf safety

--- scripts/shellConf.sh
+++ scripts/shellConf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GITPATH=$(cd $(dirname $(realpath "$0")) && cd ../ && pwd)
+GITPATH=$(cd "$(dirname "$(realpath "$0")")" && cd ../ && pwd)
 
 lnHomeConf() {
     DIR=$1
@@ -10,13 +10,13 @@ lnHomeConf() {
     ln -sv "$GITPATH/$FILE" "$DIR/$FILE"
 }
 
-lnHomeConf $HOME .bashrc
-lnHomeConf $HOME/.bash aliases
-lnHomeConf $HOME/.bash functions
-lnHomeConf $HOME/.bash prompt
-lnHomeConf $HOME .zshrc
-lnHomeConf $HOME/.zsh aliases
-lnHomeConf $HOME/.zsh functions
-lnHomeConf $HOME/.zsh prompt
+lnHomeConf "$HOME" .bashrc
+lnHomeConf "$HOME/.bash" aliases
+lnHomeConf "$HOME/.bash" functions
+lnHomeConf "$HOME/.bash" prompt
+lnHomeConf "$HOME" .zshrc
+lnHomeConf "$HOME/.zsh" aliases
+lnHomeConf "$HOME/.zsh" functions
+lnHomeConf "$HOME/.zsh" prompt
 
 printf "All bash and zsh files have been linked. Please run . ~/.bashrc for bash or . ~/.zshrc for zsh or logout and back in to enable the configs.\n"
