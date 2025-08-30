9a70a956c1d96a8c25afda05386c39c2bc644dcc
Trihedraf <forrest@mailfsh.com>
2025-08-30 13:53:04 -0700

    fix shell paths #2

--- scripts/shellConf.sh
+++ scripts/shellConf.sh
@@ -3,20 +3,21 @@ GITPATH=$(cd "$(dirname "$(realpath "$0")")" && cd ../ && pwd)
 
 lnHomeConf() {
     DIR=$1
-    FILE=$2
+    SUBDIR=$2
+    FILE=$3
     mkdir -pv "$DIR"
-    [ ! -L "$DIR/$FILE" ] || rm -v "$DIR/$FILE"
-    [ ! -f "$DIR/$FILE" ] || mv -v "$DIR/$FILE" "$DIR/$FILE.bak"
-    ln -sv "$GITPATH/$FILE" "$DIR/$FILE"
+    [ ! -L "$DIR/$SUBDIR$FILE" ] || rm -v "$DIR/$SUBDIR$FILE"
+    [ ! -f "$DIR/$SUBDIR$FILE" ] || mv -v "$DIR/$SUBDIR$FILE" "$DIR/$SUBDIR$FILE.bak"
+    ln -sv "$GITPATH/$SUBDIR$FILE" "$DIR/$SUBDIR$FILE"
 }
 
-lnHomeConf "$HOME" .bashrc
-lnHomeConf "$HOME" .bash/aliases
-lnHomeConf "$HOME" .bash/functions
-lnHomeConf "$HOME" .bash/prompt
-lnHomeConf "$HOME" .zshrc
-lnHomeConf "$HOME" .zsh/aliases
-lnHomeConf "$HOME" .zsh/functions
-lnHomeConf "$HOME" .zsh/prompt
+lnHomeConf "$HOME" "" .bashrc
+lnHomeConf "$HOME" ".bash/" aliases
+lnHomeConf "$HOME" ".bash/" functions
+lnHomeConf "$HOME" ".bash/" prompt
+lnHomeConf "$HOME" "".zshrc
+lnHomeConf "$HOME" ".zsh/" aliases
+lnHomeConf "$HOME" ".zsh/" functions
+lnHomeConf "$HOME" ".zsh/" prompt
 
 printf "All bash and zsh files have been linked. Please run . ~/.bashrc for bash or . ~/.zshrc for zsh or logout and back in to enable the configs.\n"
