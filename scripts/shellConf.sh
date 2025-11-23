1a315d94de06ab6b7924bacdf1c210b30210e018
Trihedraf <forrest@mailfsh.com>
2025-11-23 04:05:23 -0800

    comment out zsh

--- scripts/shellConf.sh
+++ scripts/shellConf.sh
@@ -9,14 +9,15 @@ lnHomeConf() {
     ln -sv "$GITPATH/$FILE" "$DIR/$FILE"
 }
 
-mkdir -pv "$HOME" "$HOME/.bash" "$HOME/.zsh"
+mkdir -pv "$HOME" "$HOME/.bash" #"$HOME/.zsh"
 lnHomeConf "$HOME" ".bashrc"
 lnHomeConf "$HOME" ".bash/aliases"
 lnHomeConf "$HOME" ".bash/functions"
 lnHomeConf "$HOME" ".bash/prompt"
-lnHomeConf "$HOME" ".zshrc"
-lnHomeConf "$HOME" ".zsh/aliases"
-lnHomeConf "$HOME" ".zsh/functions"
-lnHomeConf "$HOME" ".zsh/prompt"
+#lnHomeConf "$HOME" ".zshrc"
+#lnHomeConf "$HOME" ".zsh/aliases"
+#lnHomeConf "$HOME" ".zsh/functions"
+#lnHomeConf "$HOME" ".zsh/prompt"
 
-printf "All bash and zsh files have been linked. Please run . ~/.bashrc for bash or . ~/.zshrc for zsh or logout and back in to enable the configs.\n"
+printf "All bash and have been linked. Please run . ~/.bashrc for bash or logout and back in to enable the configs.\n"
+#printf "All bash and zsh files have been linked. Please run . ~/.bashrc for bash or . ~/.zshrc for zsh or logout and back in to enable the configs.\n"
