6f3aaf3056e43ac9ff1abc7540902effaee0ae2b
Trihedraf <forrest@mailfsh.com>
2026-03-01 08:25:29 -0800

    Create proxmox.sh
    For use with proxmox using the base debian as a server, but getting the zfs-on-root of proxmox

---
+++ proxmox.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo sh -c 'echo "" > /etc/motd'
+
+sudo dpkg --add-architecture i386
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nala || exit
+sudo DEBIAN_FRONTEND=noninteractive nala install -y \
+build-essential \
+cmake \
+fastfetch \
+git \
+gh \
+iperf3 \
+multitail \
+net-tools \
+pipx \
+screen \
+shellcheck \
+smpq \
+trash-cli \
+tree \
+unzip \
+zip \
+|| exit
+
+if ! command -v docker > /dev/null 2>&1; then
+    curl -fsSL get.docker.com | sh && sudo usermod -aG docker "$(whoami)"
+fi
+
+if ! command -v tailscale > /dev/null 2>&1; then
+    curl -fsSL https://tailscale.com/install.sh | sh
+fi
+
+if git clone https://github.com/Trihedraf/linux.confs "$HOME/git/linux.confs"; then
+    "$HOME/git/linux.confs/scripts/spfInstall.sh"
+    "$HOME/git/linux.confs/scripts/configFiles.sh" -t || printf "terminal app configurations failed"
+    "$HOME/git/linux.confs/scripts/shellConf.sh" || printf "shell configuration failed"
+    if cd "$HOME/git/linux.confs"; then
+        if sudo cp -rv ./debian-trixie/etc/* /etc/; then
+            sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+            sudo systemctl enable udpgroforwarding.service
+            sudo rm /etc/apt/sources.list && sudo nala update
+        fi
+    fi
+fi
+
+echo "Please change your NIC from eth0 to correct id in /etc/systemd/system/udpgroforwarding.service then start udpgroforwarding.service"
